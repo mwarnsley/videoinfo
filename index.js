10d479fc1b1f92c5e9da5c812243d13b18d3f94f
Marcus Warnsley <marcus@Marcuss-MacBook-Air.local>
2018-06-13 22:37:20 -0400

    Adding in the start command and checking to make sure the ready function is working

--- index.js
+++ index.js
@@ -3,4 +3,11 @@
  */
 
 // Import the required modules using the Node.js syntax
-const electron = require("electron");
+const electron = require('electron');
+// Lifecycle of the running electron application
+const { app } = electron;
+
+// Running the ready event on the app
+app.on('ready', () => {
+  console.log('The App is now ready');
+});
