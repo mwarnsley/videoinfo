3711eb36f3aa4b2899d6f8067f1389c42f598b6c
Marcus Warnsley <marcus@Marcuss-MacBook-Air.local>
2018-06-14 00:12:19 -0400

    Finishing up the small application for displaying the video duration

--- index.js
+++ index.js
@@ -4,13 +4,28 @@
 
 // Import the required modules using the Node.js syntax
 const electron = require('electron');
+const ffmpeg = require('fluent-ffmpeg');
+
 // Lifecycle of the running electron application
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, ipcMain } = electron;
+
+let mainWindow;
 
 // Running the ready event on the app
 app.on('ready', () => {
   // Starting a new browser window for the application
-  const mainWindow = new BrowserWindow({});
+  mainWindow = new BrowserWindow({});
+
   // Telling the window to go out and load up an html document
   mainWindow.loadURL(`file://${__dirname}/index.html`);
+});
+
+// Listening for the message from the main window of the application
+ipcMain.on('video:submit', (event, path) => {
+  ffmpeg.ffprobe(path, (err, metadata) => {
+    const duration = metadata.format.duration;
+
+    // Sending the data back to the main window
+    mainWindow.webContents.send('video:metadata', duration);
+  });
 });
